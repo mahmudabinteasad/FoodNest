08bae6895e7a31a4b30f5808e2041051cc5f9d56
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2025-05-05 03:23:22 +0600

    Update admin.py

--- foodapp/admin.py
+++ foodapp/admin.py
@@ -3,7 +3,12 @@
 from django.contrib import admin
 from .models import Restaurant, MenuItem, Order, OrderDetail
 
-admin.site.register(Restaurant)
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_featured', 'address', 'phone')
+    list_filter = ('is_featured',)
+    search_fields = ('name', 'address')
+    
 admin.site.register(MenuItem)
 admin.site.register(Order)
 admin.site.register(OrderDetail)
