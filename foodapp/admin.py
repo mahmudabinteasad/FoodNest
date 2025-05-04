9dcbcb3f6403bc5d6ccbb542af7e70cb96cc926f
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2025-05-05 00:23:49 +0600

    Delete foodapp directory

--- foodapp/admin.py
+++
@@ -1,9 +0,0 @@
-# foodapp/admin.py
-
-from django.contrib import admin
-from .models import Restaurant, MenuItem, Order, OrderDetail
-
-admin.site.register(Restaurant)
-admin.site.register(MenuItem)
-admin.site.register(Order)
-admin.site.register(OrderDetail)
