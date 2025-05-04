463421703f9c972b1ae35ec558e03c3178006f47
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2025-05-05 03:17:08 +0600

    Update urls.py

--- foodnest/urls.py
+++ foodnest/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', include('foodapp.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='foodapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='foodapp/logout.html'), name='logout'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
