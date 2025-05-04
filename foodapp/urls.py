d7293b109533bcf6874e2361e1a03733f241cf0a
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2025-05-05 03:21:10 +0600

    Update urls.py

--- foodapp/urls.py
+++ foodapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('restaurants/', views.restaurant_list, name='restaurant_list'),
     path('restaurant/<int:restaurant_id>/', views.menu_item_list, name='menu_item_list'),
     path('register/', views.register, name='register'),
+    path('login/', views.custom_login_view, name='login'),  # using custom login function-based view
 ]
